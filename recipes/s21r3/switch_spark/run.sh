40cbb46b78d432e6d2d31120f79064041a5f1bd8
The BSD Professor <me@bsdprof.com>
2021-06-02 20:09:54 +0200

    Recipes to switch the course environment into the right setup for the course

--- recipes/s21r3/switch_spark/run.sh
+++ recipes/s21r3/switch_spark/run.sh
@@ -5,39 +5,39 @@ OSBDET_LOGFILE=$OSBDET_HOME/osbdet.log
 SCRIPT_PATH=$(dirname $(realpath $0))
 
 build_module() {
-  echo "switch_ibda.build_module DEBUG [`date +"%Y-%m-%d %T"`] Building module '$1'" >> $OSBDET_LOGFILE
+  echo "switch_spark.build_module DEBUG [`date +"%Y-%m-%d %T"`] Building module '$1'" >> $OSBDET_LOGFILE
   IS_INSTALLED=`$OSBDET_HOME/osbdet_builder.sh status | grep "$1" | grep KO | wc -l`
 
   if [ "$IS_INSTALLED" == "0" ]
   then
-    echo "switch_ibda.build_module DEBUG [`date +"%Y-%m-%d %T"`] Module '$1' is already built" >> $OSBDET_LOGFILE
+    echo "switch_spark.build_module DEBUG [`date +"%Y-%m-%d %T"`] Module '$1' is already built" >> $OSBDET_LOGFILE
     printf "Skipped [Already installed]\n"
   else
     printf "Building "
     $OSBDET_HOME/osbdet_builder.sh build $1 > /dev/null 2>&1
     printf "[Build]\n"
-    echo "switch_ibda.build_module DEBUG [`date +"%Y-%m-%d %T"`] Module '$1' has been built" >> $OSBDET_LOGFILE
+    echo "switch_spark.build_module DEBUG [`date +"%Y-%m-%d %T"`] Module '$1' has been built" >> $OSBDET_LOGFILE
   fi
 }
 
 remove_module() {
-  echo "switch_ibda.remove_module DEBUG [`date +"%Y-%m-%d %T"`] Removing module '$1'" >> $OSBDET_LOGFILE
+  echo "switch_spark.remove_module DEBUG [`date +"%Y-%m-%d %T"`] Removing module '$1'" >> $OSBDET_LOGFILE
   IS_INSTALLED=`$OSBDET_HOME/osbdet_builder.sh status | grep "$1" | grep KO | wc -l`
 
   if [ "$IS_INSTALLED" == "1" ]
   then
-    echo "switch_ibda.remove_module DEBUG [`date +"%Y-%m-%d %T"`] Module '$1' is already removed" >> $OSBDET_LOGFILE
+    echo "switch_spark.remove_module DEBUG [`date +"%Y-%m-%d %T"`] Module '$1' is already removed" >> $OSBDET_LOGFILE
     printf "Skipped [Not available]\n"
   else
     printf "Removing "
     $OSBDET_HOME/osbdet_builder.sh remove $1 > /dev/null 2>&1
     printf "[Removed]\n"
-    echo "switch_ibda.remove_module DEBUG [`date +"%Y-%m-%d %T"`] Module '$1' has been removed" >> $OSBDET_LOGFILE
+    echo "switch_spark.remove_module DEBUG [`date +"%Y-%m-%d %T"`] Module '$1' has been removed" >> $OSBDET_LOGFILE
   fi
 }
 
 build_modules() {
-  MODULES=(foundation jupyter hadoop3 hive3 nifi superset)
+  MODULES=(foundation jupyter spark3 truckssim mongodb44)
 
   for module in ${MODULES[@]}
   do
@@ -47,7 +47,7 @@ build_modules() {
 }
 
 remove_modules() {
-  MODULES=(mongodb44 mariadb truckssim kafka2 spark3)
+  MODULES=(hadoop3 hive3 mariadb kafka2 nifi superset)
 
   for module in ${MODULES[@]}
   do
@@ -56,13 +56,13 @@ remove_modules() {
   done
 }
 
-echo "switch_ibda DEBUG [`date +"%Y-%m-%d %T"`] Start cooking the 'switch_ibda' recipe" >> $OSBDET_LOGFILE
-echo "Running 'switch_ibda' recipe:"
+echo "switch_spark DEBUG [`date +"%Y-%m-%d %T"`] Start cooking the 'switch_spark' recipe" >> $OSBDET_LOGFILE
+echo "Running 'switch_spark' recipe:"
 echo
 echo "  - Building the right modules if needed... "
 build_modules
 echo "  - Removing unneeded modules ... "
 remove_modules
 echo
-echo "'switch_ibda' has been executed"
-echo "switch_ibda DEBUG [`date +"%Y-%m-%d %T"`] The 'switch_ibda' has been cooked" >> $OSBDET_LOGFILE
+echo "'switch_spark' has been executed"
+echo "switch_spark DEBUG [`date +"%Y-%m-%d %T"`] The 'switch_spark' has been cooked" >> $OSBDET_LOGFILE
--- recipes/s21r3/switch_streaming/run.sh
+++ recipes/s21r3/switch_streaming/run.sh
@@ -5,39 +5,39 @@ OSBDET_LOGFILE=$OSBDET_HOME/osbdet.log
 SCRIPT_PATH=$(dirname $(realpath $0))
 
 build_module() {
-  echo "switch_ibda.build_module DEBUG [`date +"%Y-%m-%d %T"`] Building module '$1'" >> $OSBDET_LOGFILE
+  echo "switch_streaming.build_module DEBUG [`date +"%Y-%m-%d %T"`] Building module '$1'" >> $OSBDET_LOGFILE
   IS_INSTALLED=`$OSBDET_HOME/osbdet_builder.sh status | grep "$1" | grep KO | wc -l`
 
   if [ "$IS_INSTALLED" == "0" ]
   then
-    echo "switch_ibda.build_module DEBUG [`date +"%Y-%m-%d %T"`] Module '$1' is already built" >> $OSBDET_LOGFILE
+    echo "switch_streaming.build_module DEBUG [`date +"%Y-%m-%d %T"`] Module '$1' is already built" >> $OSBDET_LOGFILE
     printf "Skipped [Already installed]\n"
   else
     printf "Building "
     $OSBDET_HOME/osbdet_builder.sh build $1 > /dev/null 2>&1
     printf "[Build]\n"
-    echo "switch_ibda.build_module DEBUG [`date +"%Y-%m-%d %T"`] Module '$1' has been built" >> $OSBDET_LOGFILE
+    echo "switch_streaming.build_module DEBUG [`date +"%Y-%m-%d %T"`] Module '$1' has been built" >> $OSBDET_LOGFILE
   fi
 }
 
 remove_module() {
-  echo "switch_ibda.remove_module DEBUG [`date +"%Y-%m-%d %T"`] Removing module '$1'" >> $OSBDET_LOGFILE
+  echo "switch_streaming.remove_module DEBUG [`date +"%Y-%m-%d %T"`] Removing module '$1'" >> $OSBDET_LOGFILE
   IS_INSTALLED=`$OSBDET_HOME/osbdet_builder.sh status | grep "$1" | grep KO | wc -l`
 
   if [ "$IS_INSTALLED" == "1" ]
   then
-    echo "switch_ibda.remove_module DEBUG [`date +"%Y-%m-%d %T"`] Module '$1' is already removed" >> $OSBDET_LOGFILE
+    echo "switch_streaming.remove_module DEBUG [`date +"%Y-%m-%d %T"`] Module '$1' is already removed" >> $OSBDET_LOGFILE
     printf "Skipped [Not available]\n"
   else
     printf "Removing "
     $OSBDET_HOME/osbdet_builder.sh remove $1 > /dev/null 2>&1
     printf "[Removed]\n"
-    echo "switch_ibda.remove_module DEBUG [`date +"%Y-%m-%d %T"`] Module '$1' has been removed" >> $OSBDET_LOGFILE
+    echo "switch_streaming.remove_module DEBUG [`date +"%Y-%m-%d %T"`] Module '$1' has been removed" >> $OSBDET_LOGFILE
   fi
 }
 
 build_modules() {
-  MODULES=(foundation jupyter hadoop3 hive3 nifi superset)
+  MODULES=(foundation jupyter truckssim kafka2 spark3 mariadb superset)
 
   for module in ${MODULES[@]}
   do
@@ -47,7 +47,7 @@ build_modules() {
 }
 
 remove_modules() {
-  MODULES=(mongodb44 mariadb truckssim kafka2 spark3)
+  MODULES=(mongodb44 hadoop3 hive3 nifi)
 
   for module in ${MODULES[@]}
   do
@@ -56,13 +56,13 @@ remove_modules() {
   done
 }
 
-echo "switch_ibda DEBUG [`date +"%Y-%m-%d %T"`] Start cooking the 'switch_ibda' recipe" >> $OSBDET_LOGFILE
-echo "Running 'switch_ibda' recipe:"
+echo "switch_streaming DEBUG [`date +"%Y-%m-%d %T"`] Start cooking the 'switch_streaming' recipe" >> $OSBDET_LOGFILE
+echo "Running 'switch_streaming' recipe:"
 echo
 echo "  - Building the right modules if needed... "
 build_modules
 echo "  - Removing unneeded modules ... "
 remove_modules
 echo
-echo "'switch_ibda' has been executed"
-echo "switch_ibda DEBUG [`date +"%Y-%m-%d %T"`] The 'switch_ibda' has been cooked" >> $OSBDET_LOGFILE
+echo "'switch_streaming' has been executed"
+echo "switch_streaming DEBUG [`date +"%Y-%m-%d %T"`] The 'switch_streaming' has been cooked" >> $OSBDET_LOGFILE
