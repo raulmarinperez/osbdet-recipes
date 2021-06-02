1fa7e83f7dbdcfd098abe5c626ba8568f04b37e0
The BSD Professor <me@bsdprof.com>
2021-06-02 19:46:44 +0200

    loggin added

--- recipes/s21r3/switch_ibda/run.sh
+++ recipes/s21r3/switch_ibda/run.sh
@@ -5,15 +5,19 @@ OSBDET_LOGFILE=$OSBDET_HOME/osbdet.log
 SCRIPT_PATH=$(dirname $(realpath $0))
 
 build_module() {
+  echo "switch_ibda.build_module DEBUG [`date +"%Y-%m-%d %T"`] Building module '$1'" >> $OSBDET_LOGFILE
   IS_INSTALLED=`$OSBDET_HOME/osbdet_builder.sh status | grep "$1" | grep KO | wc -l`
 
   if [ "$IS_INSTALLED" == "0" ]
   then
+    echo "switch_ibda.build_module DEBUG [`date +"%Y-%m-%d %T"`] Module '$1' is already built" >> $OSBDET_LOGFILE
     printf "Skipped [Already installed]\n"
   else
+    echo "switch_ibda.build_module DEBUG [`date +"%Y-%m-%d %T"`] Building module '$1'" >> $OSBDET_LOGFILE
     printf "Building\n"
     $OSBDET_HOME/osbdet_builder.sh build $1
     printf "[Build]\n"
+    echo "switch_ibda.build_module DEBUG [`date +"%Y-%m-%d %T"`] Module '$1' has been built" >> $OSBDET_LOGFILE
   fi
 }
 
@@ -31,6 +35,7 @@ remove_modules() {
    echo "Removing modules"
 }
 
+echo "switch_ibda DEBUG [`date +"%Y-%m-%d %T"`] Start cooking the 'switch_ibda' recipe" >> $OSBDET_LOGFILE
 echo "Running 'switch_ibda' recipe:"
 echo
 echo "  - Building the right modules if needed... "
@@ -39,3 +44,4 @@ echo "  - Removing unneeded modules ... "
 remove_modules
 echo
 echo "'switch_ibda' has been executed"
+echo "switch_ibda DEBUG [`date +"%Y-%m-%d %T"`] The 'switch_ibda" has been cooked" >> $OSBDET_LOGFILE
